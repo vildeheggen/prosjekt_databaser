5395cee1ea64698921f832b3f87941562ea58c42
Silje Marie Anfindsen <siljeanf@stud.ntnu.no>
2021-03-19 17:49:55 +0100

    fiksa litt på loggin med databasen

--- db2_code/src/LogInCtrl.java
+++ db2_code/src/LogInCtrl.java
@@ -9,18 +9,22 @@ public class LogInCtrl extends dbconn{
         Scanner sc2= new Scanner(System.in);
         System.out.print("Enter password: ");  
         String password= sc2.nextLine();
-        System.out.println(email.length());
-        System.out.println(password.length());
+        //System.out.println(email.length());
+        //System.out.println(password.length());
+        String query = "select * from User where Email = '" + email + "' and Password = '" + password + "'";
+        System.out.println(query);
 
         try { 
             Statement stmt = conn.createStatement();
-            String query = "select * from Users where Email='email' and Password='password'";
+            //String query = "select * from User where Email = " + email + " and Password = " + password;
+            //System.out.println(query);
             ResultSet rs = stmt.executeQuery(query);
+            System.out.println(rs);
             if (rs != null){System.out.print("Correct login");  }
             else {System.out.print("Wrong login");  }
 
         } catch (Exception e) { 
-            System.out.println("db error during select of Users = "+e);
+            System.out.println("db error during select of User = "+e);
         }
     }
 }
