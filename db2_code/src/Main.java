7fbabdd1ceada3eae0890c0b91f5a899a9e42bec
Silje Marie Anfindsen <siljeanf@stud.ntnu.no>
2021-03-25 21:45:48 +0100

    fikset course / member check, men funker ikke 100% så skal fikse mer

--- db2_code/src/Main.java
+++ db2_code/src/Main.java
@@ -4,24 +4,22 @@ public class Main {
         LogInCtrl loginCtrl = new LogInCtrl();
         loginCtrl.connect();
         String confirmedEmail = loginCtrl.login();
-        
        
-        //makePostCtrl makepost = new makePostCtrl(confirmedEmail);
-        //makepost.connect();
-        //makepost.makePost();
+        makePostCtrl makepost = new makePostCtrl(confirmedEmail);
+        makepost.connect();
+        makepost.makePost();
 
-        replyCtrl reply1 = new replyCtrl(confirmedEmail);
-        reply1.connect();
-        reply1.reply();
+        // replyCtrl rseply1 = new replyCtrl(confirmedEmail);
+        // reply1.connect();
+        // reply1.reply();
 
-        //SearchCtrl search = new SearchCtrl();
-        //search.connect();
-        //search.findPostsWithKeyWord();
+        // SearchCtrl search = new SearchCtrl();
+        // search.connect();
+        // search.findPostsWithKeyWord();
 
-        //ViewStatistics statistics = new ViewStatistics();
-        //statistics.connect();
-        //statistics.doQueries();
+        // ViewStatistics statistics = new ViewStatistics();
+        // statistics.connect();
+        // statistics.doQueries();
 
     }
-
 }
