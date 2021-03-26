00343383d532b6e7dd3e0596451861346ea1a3df
Silje Marie Anfindsen <siljeanf@stud.ntnu.no>
2021-03-26 23:21:13 +0100

    fikset main

--- db2_code/src/Main.java
+++ db2_code/src/Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner; // Import the Scanner class
 public class Main {
 
         public static void main(String[] args) {
-                while (true) {
+                loop: while (true) {
                         // Usecase 1: (kreves for å kjøre usecase 2, 3 og 5)
                         System.out.println("Welcome to piazza!");
                         System.out.println("Please log in.");
@@ -11,17 +11,17 @@ public class Main {
                         loginCtrl.connect();
                         String confirmedEmail = loginCtrl.login();
 
-                        boolean loggedIn = true;
+                        Boolean loggedIn = true;
 
                         while (loggedIn) {
-                                System.out.print("Choose the usecase you want to run (2,3,4,5): ");
+                                System.out.print("Choose the usecase you want to run (2,3,4,5). Press 1 to log out.");
                                 Scanner sc = new Scanner(System.in);
                                 Integer usecase = sc.nextInt();
-                                sc.close();
 
                                 switch (usecase) {
                                 case 1:  // Logg ut (altså gjør usecase 1 på nytt)
                                         loggedIn = false;
+                                        break loop;
                                 case 2: // Usecase 2:
                                         makePostCtrl makepost = new makePostCtrl(confirmedEmail);
                                         makepost.connect();
